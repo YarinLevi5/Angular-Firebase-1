e218c2723add036ba4b167d38fc8995720f148e2
yarin <yarin066@gmail.com>
2022-04-30 23:58:16 +0300

    Angular and Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDt601ZjwuB2A-ZBqPwDIuDc1y42fW_umE",
+    authDomain: "angular-firebase-1-46aa0.firebaseapp.com",
+    projectId: "angular-firebase-1-46aa0",
+    storageBucket: "angular-firebase-1-46aa0.appspot.com",
+    messagingSenderId: "560655656420",
+    appId: "1:560655656420:web:b4c6654f156340f3efd3f1",
+    measurementId: "G-LJGVH7M4TS"
+  }
 };
